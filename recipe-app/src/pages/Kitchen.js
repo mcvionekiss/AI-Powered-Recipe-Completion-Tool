e96a3a1b9dacdb55bd94ea8b364901a7d83e8963
Axel Lopez <117297366+Weasel187ea@users.noreply.github.com>
2025-05-01 20:13:14 -0700

    added css file

--- recipe-app/src/pages/Kitchen.js
+++ recipe-app/src/pages/Kitchen.js
@@ -2,6 +2,8 @@ import React from 'react';
 import RegularSidebar from '../components/RegularSidebar';
 import ProfileButton from '../components/ProfileButton';
 import { Box } from '@mui/material';
+import filterIcon from './filter.png';
+import './Kitchen.css'; // Import the CSS file for styling
 
 
 // kitchen page
@@ -14,10 +16,33 @@ const Kitchen = () => {
           <h1>Your Kitchen</h1>
           <hr style={{ marginTop: '16px', marginBottom: '16px', border: 'none', borderTop: '2px solid #ccc' }} />
         </Box>
+        <Box size="large" sx={{ display: 'flex', justifyContent: 'space-between', mt: 2 }}>
+          <button id ="filter">
+            <img src={filterIcon} alt= "filter icon" style={{
+               width: '16px', height: '16px',
+               borderRadius: '50%',
+               border: '2px solidrgb(0, 0, 0)',
+               padding: '4px',              
+               backgroundColor: 'white'    
+               }} />
+            Filter
+          </button>
+          <button id="generateRecipe">
+            Generate Recipe
+          </button>
+
+        </Box>
+
       </Box>
+      
       <ProfileButton />
     </Box>
+
+    
+
+
   );
 };
+
 
 export default Kitchen;
